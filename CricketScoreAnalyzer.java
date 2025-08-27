95924a39fc8f22ee0c586735b4db1631aeb40724
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2025-08-27 15:30:12 +0530

    addfiles

---
+++ CricketScoreAnalyzer.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class CricketScoreAnalyzer {
+
+    // Method to calculate total runs
+    public static int getTotalRuns(int[] runs) {
+        int total = 0;
+        for (int run : runs) {
+            total += run;
+        }
+        return total;
+    }
+
+    
+    public static double getAverageRuns(int[] runs) {
+        return (double) getTotalRuns(runs) / runs.length;
+    }
+
+    public static int getHighestOver(int[] runs) {
+        int maxRuns = runs[0];
+        int over = 1;
+        for (int i = 1; i < runs.length; i++) {
+            if (runs[i] > maxRuns) {
+                maxRuns = runs[i];
+                over = i + 1; 
+            }
+        }
+        return over;
+    }
+
+   
+    public static int getLowestOver(int[] runs) {
+        int minRuns = runs[0];
+        int over = 1;
+        for (int i = 1; i < runs.length; i++) {
+            if (runs[i] < minRuns) {
+                minRuns = runs[i];
+                over = i + 1;
+            }
+        }
+        return over;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of overs: ");
+        int n = sc.nextInt();
+        int[] runs = new int[n];
+
+        System.out.println("Enter runs scored in each over:");
+        for (int i = 0; i < n; i++) {
+            runs[i] = sc.nextInt();
+        }
+
+       
+        int total = getTotalRuns(runs);
+        double avg = getAverageRuns(runs);
+        int highOver = getHighestOver(runs);
+        int lowOver = getLowestOver(runs);
+
+       
+        System.out.println("\n--- Cricket Score Analysis ---");
+        System.out.println("Total Runs: " + total);
+        System.out.println("Average Runs per Over: " + avg);
+        System.out.println("Highest scoring over: Over " + highOver + " (" + runs[highOver-1] + " runs)");
+        System.out.println("Lowest scoring over: Over " + lowOver + " (" + runs[lowOver-1] + " runs)");
+    }
+}
