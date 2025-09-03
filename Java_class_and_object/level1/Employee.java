b22b0adb9ef4ea17b9ff6c6fc2d6f21ef4717237
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2025-09-03 12:41:34 +0530

    add program for employee details

---
+++ Java_class_and_object/level1/Employee.java
@@ -0,0 +1,20 @@
+// package Java_class_and_object.level1;
+
+public class Employee {
+     String name;
+     int id;
+     double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee Details:");
+        System.out.println("Name   : " + name);
+        System.out.println("ID     : " + id);
+        System.out.println("Salary : $" + salary);
+    }
+}
---
+++ Java_class_and_object/level1/EmployeeMain.java
@@ -0,0 +1,7 @@
+// Main.java
+public class EmployeeMain{
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("rishu", 55, 55000);
+        emp1.displayDetails();
+    }
+}
