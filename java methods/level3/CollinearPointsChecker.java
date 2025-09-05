2308a9df368d9e02e73358a98f76df546374117d
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2025-09-05 23:02:56 +0530

    add program

---
+++ java methods/level3/CollinearPointsChecker.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class CollinearPointsChecker {
+
+    // Method to check collinearity using slope formula
+    public static boolean areCollinearSlope(int x1, int y1, int x2, int y2, int x3, int y3) {
+        // Avoid division by zero by cross multiplication
+        return (y2 - y1) * (x3 - x2) == (y3 - y2) * (x2 - x1) &&
+               (y2 - y1) * (x3 - x1) == (y3 - y1) * (x2 - x1);
+    }
+
+    // Method to check collinearity using area of triangle formula
+    public static boolean areCollinearArea(int x1, int y1, int x2, int y2, int x3, int y3) {
+        double area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+        return area == 0;
+    }
+
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+        System.out.print("Enter x1 y1: ");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+
+        System.out.print("Enter x2 y2: ");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        System.out.print("Enter x3 y3: ");
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+
+       
+        boolean collinearSlope = areCollinearSlope(x1, y1, x2, y2, x3, y3);
+       
+        boolean collinearArea = areCollinearArea(x1, y1, x2, y2, x3, y3);
+
+        System.out.println("\nUsing Slope Method: " + (collinearSlope ? "Points are Collinear" : "Points are NOT Collinear"));
+        System.out.println("Using Area Method: " + (collinearArea ? "Points are Collinear" : "Points are NOT Collinear"));
+
+        sc.close();
+    }
+}
+
