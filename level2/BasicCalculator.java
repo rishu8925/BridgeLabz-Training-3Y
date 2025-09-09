88d82cf9b8a80675d8329dcf62154c0969e9efc3
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2025-09-09 16:11:17 +0530

    add program

---
+++ level2/BasicCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take user input
+        System.out.print("Enter the first number: ");
+        double number1 = sc.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double number2 = sc.nextDouble();
+
+        // Perform operations
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = 0;
+
+        if (number2 != 0) {
+            division = number1 / number2;
+        } else {
+            System.out.println("⚠ Division by zero is not allowed. Division set to 0.");
+        }
+
+        // Output
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+                + number1 + " and " + number2 + " is "
+                + addition + ", " + subtraction + ", "
+                + multiplication + ", " + division);
+    }
+}
+
+
