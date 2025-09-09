fc66a85bc4f993e4f1a3003395498e6693f296e1
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2025-09-09 16:24:00 +0530

    add program

---
+++ level2/QuotientRemainder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int number1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int number2 = sc.nextInt();
+
+        int quotient = number1 / number2;
+        int remainder = number1 % number2;
+
+        System.out.printf("The Quotient is %d and Remainder is %d of two numbers %d and %d%n", quotient, remainder, number1, number2);
+        sc.close();
+    }
+}
+
