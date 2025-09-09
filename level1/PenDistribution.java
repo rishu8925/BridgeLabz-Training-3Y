ea8081aa105787712bc798f090d075c1d9614657
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2025-09-09 14:58:53 +0530

    add program

---
+++ level1/PenDistribution.java
@@ -0,0 +1,13 @@
+public class PenDistribution {
+    public static void main(String[] args) {
+        int totalPens = 14;
+        int students = 3;
+
+        int pensPerStudent = totalPens / students;   // Division operator
+        int remainingPens = totalPens % students;    // Modulus operator
+
+        System.out.println("The Pen Per Student is " + pensPerStudent +
+                           " and the remaining pen not distributed is " + remainingPens);
+    }
+}
+
