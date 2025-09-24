14e635d021cafa98d60968637e66e804a34e0fba
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2025-09-24 13:48:47 +0530

    add files

---
+++ Handling strings/PalindromeChecker.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        String formatted = input.replaceAll("\\s+", "").toLowerCase();
+
+        if (isPalindrome(formatted)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
