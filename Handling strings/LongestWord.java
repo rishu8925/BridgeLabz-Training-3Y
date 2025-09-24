d806babc77112b1b23e96d8c10794486311da67c
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2025-09-24 13:52:50 +0530

    add files

---
+++ Handling strings/LongestWord.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LongestWord {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+        System.out.println("Longest word: " + findLongestWord(sentence));
+        scanner.close();
+    }
+
+    public static String findLongestWord(String sentence) {
+        String[] words = sentence.split("\\s+");
+        String longest = "";
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+        return longest;
+    }
+}
